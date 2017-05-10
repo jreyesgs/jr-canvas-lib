01a6a6bf8b83a2e9bf54dbcd872988ebad4596ce
Jose Reyes <lab@jreyes.es>
2017-05-10 16:22:30 +0200

    Añadida funciones para aclarar y oscurecer colores en hexadecimal

--- jr-creative-coding.js
+++ jr-creative-coding.js
@@ -98,3 +98,34 @@ var infiniteColor = function(){
 // radius
 //
 // numVertex
+
+
+/* Suma el porcentaje indicado a un color (RR, GG o BB) hexadecimal para aclararlo */
+var addLight = function addLight(color, amount) {
+  var cc = parseInt(color, 16) + amount;
+  var c = cc > 255 ? 255 : cc;
+  c = c.toString(16).length > 1 ? c.toString(16) : "0" + c.toString(16);
+  return c;
+};
+
+/* Aclara un color hexadecimal de 6 caracteres #RRGGBB segun el porcentaje indicado */
+var lighten = function lighten(color, amount) {
+  color = color.indexOf("#") >= 0 ? color.substring(1, color.length) : color;
+  amount = parseInt(255 * amount / 100);
+  return color = "#" + addLight(color.substring(0, 2), amount) + addLight(color.substring(2, 4), amount) + addLight(color.substring(4, 6), amount);
+};
+
+/* Resta el porcentaje indicado a un color (RR, GG o BB) hexadecimal para oscurecerlo */
+var subtractLight = function subtractLight(color, amount) {
+  var cc = parseInt(color, 16) - amount;
+  var c = cc < 0 ? 0 : cc;
+  c = c.toString(16).length > 1 ? c.toString(16) : "0" + c.toString(16);
+  return c;
+};
+
+/* Oscurece un color hexadecimal de 6 caracteres #RRGGBB segun el porcentaje indicado */
+var darken = function darken(color, amount) {
+  color = color.indexOf("#") >= 0 ? color.substring(1, color.length) : color;
+  amount = parseInt(255 * amount / 100);
+  return color = "#" + subtractLight(color.substring(0, 2), amount) + subtractLight(color.substring(2, 4), amount) + subtractLight(color.substring(4, 6), amount);
+};
